a3635435ab98c33ca2276107dec04d3da2034afc
thidoa <thiagovini200@gmail.com>
2022-07-04 20:26:48 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .models import Produto
+
+def index(request):
+    produtos = Produto.objects.all()
+    context = {
+        'curso': 'Programação Web com Django Framework',
+        'outro': 'Django é massa',
+        'produtos': produtos
+    }
+    return render(request, 'index.html', context)
+
+def contato(request):
+    return render(request, 'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+
+    context = {
+        'produto': prod,
+    }
+    return render(request, 'produto.html', context)
