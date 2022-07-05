c3fdd44ef8db3521a64cc0e67759164a691a6bd8
thidoa <thiagovini200@gmail.com>
2022-07-05 15:53:15 -0300

    Projeto finalizado

--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from django.http import HttpResponse
+from django.template import loader
+
 from .models import Produto
 
 def index(request):
@@ -14,9 +18,18 @@ def contato(request):
     return render(request, 'contato.html')
 
 def produto(request, pk):
-    prod = Produto.objects.get(id=pk)
+    #prod = Produto.objects.get(id=pk)
+    prod = get_object_or_404(Produto, id=pk)
 
     context = {
         'produto': prod,
     }
     return render(request, 'produto.html', context)
+
+def error404(request, ex):
+    template = loader.get_template('404.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=404)
+
+def error500(request):
+    template = loader.get_template('500.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=500)
--- django1/settings.py
+++ django1/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hxt0c2*i_ltl)_f$mv@tbrg7_!iv70!nxa)0t$n55ykomdmb_r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #DEBUG com valor True = modo desenvulvimento
 #DEBUG com valor False = modo produção
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*'] 
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +127,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # usado durante a produção
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'index'
