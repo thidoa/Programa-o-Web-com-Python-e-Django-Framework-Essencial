35ab11d8a3073139b3c8aa96dff3ea4456af24f5
thidoa <thiagovini200@gmail.com>
2022-07-09 06:49:08 -0300

    testando

--- core/admin.py
+++ core/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from .models import Produto, Cliente
 
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'preco', 'estoque')
+    list_display = ('nome', 'preco', 'estoque',)
 
 class ClienteAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'sobrenome', 'email')
+    list_display = ('nome', 'sobrenome', 'email',)
 
 admin.site.register(Produto, ProdutoAdmin)
 admin.site.register(Cliente, ClienteAdmin)
